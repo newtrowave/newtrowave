7348ec6b3dff1e3858f825bb6e39d19b29f0491c
yeonghyeon <hah_528@naver.com>
2020-02-07 18:04:01 +0900

    로그인, Posts 추가

---
+++ src/main/java/com/kostars/newtroshop/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.kostars.newtroshop.config.auth;
+
+import com.kostars.newtroshop.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/img/**",  "/css/**", "/images/**", "/js/**", "/lib/**", "/style.css", "/fonts/**", "/loginpage", "/h2-console/**").permitAll()
+                    .antMatchers("/admin/**").hasRole(Role.ADMIN.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                     .defaultSuccessUrl("/", true)
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
